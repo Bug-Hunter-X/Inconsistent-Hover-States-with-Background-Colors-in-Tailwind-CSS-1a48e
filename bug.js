fa4e12009aea77013f36e45cce7d1647f62da97c
BugHunter-X <askzenai@gmail.com>
2024-12-24 09:48:04 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,8 @@
+```javascript
+// Example showing an incorrect use of Tailwind classes that can lead to unexpected behavior.
+<div class="bg-red-500 p-4 text-white">
+  <button class="hover:bg-blue-500">
+    Click Me
+  </button>
+</div>
+```
